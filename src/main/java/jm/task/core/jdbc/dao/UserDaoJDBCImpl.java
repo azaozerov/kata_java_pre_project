04b777b32a4ad51e65f98d892aa93244b97a290b
Artem <azaozerov@gmail.com>
2022-12-29 16:34:32 +0500

    version 0.3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,26 +1,23 @@
 package jm.task.core.jdbc.dao;
 import java.sql.*;
-import java.text.MessageFormat;
 import java.util.*;
 import jm.task.core.jdbc.util.*;
 import jm.task.core.jdbc.model.*;
 
 
 public class UserDaoJDBCImpl implements UserDao {
-    private String usersTableName = "my_users";
 
     public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-        String createTableSQL = MessageFormat.format(
-                """
-                        CREATE TABLE IF NOT EXISTS {0} (
-                            id          bigint  NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                            name        varchar(100) NOT NULL,
-                            last_name   varchar(100) NOT NULL,
-                            age         tinyint NOT NULL)
-                      """, usersTableName);
+        String createTableSQL = """
+                                    CREATE TABLE IF NOT EXISTS my_users (
+                                        id          bigint  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                        name        varchar(100) NOT NULL,
+                                        last_name   varchar(100) NOT NULL,
+                                        age         tinyint NOT NULL)
+                               """;
         try (Connection connection = Util.getMySQLConnection();
             Statement stmt = connection.createStatement()) {
             stmt.execute(createTableSQL);
@@ -30,7 +27,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void dropUsersTable() {
-        String dropTableSQL = MessageFormat.format("DROP TABLE IF EXISTS {0}", usersTableName);
+        String dropTableSQL = "DROP TABLE IF EXISTS my_users";
         try (Connection connection = Util.getMySQLConnection();
             Statement stmt = connection.createStatement()) {
             stmt.execute(dropTableSQL);
@@ -40,30 +37,34 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String insertUserSQL = MessageFormat.format("INSERT INTO {0}(name, last_name, age) VALUES (\"{1}\", \"{2}\", {3})", usersTableName, name, lastName, age) ;
+        String insertUserSQL = "INSERT INTO my_users(name, last_name, age) VALUES (?, ?, ?)";
         try (Connection connection = Util.getMySQLConnection();
             PreparedStatement stmt = connection.prepareStatement(insertUserSQL)) {
-            stmt.executeUpdate(insertUserSQL);
+            stmt.setString(1, name);
+            stmt.setString(2, lastName);
+            stmt.setByte(3, age);
+            stmt.executeUpdate();
         } catch (SQLException sqlException) {
             errorOut(sqlException);
         }
     }
 
     public void removeUserById(long id) {
-        String deleteUserSQL = MessageFormat.format("DELETE FROM {0} WHERE id = {1}", usersTableName, id);
+        String deleteUserSQL = "DELETE FROM my_users WHERE id = ?";
         try (Connection connection = Util.getMySQLConnection();
             PreparedStatement stmt = connection.prepareStatement(deleteUserSQL)) {
-            stmt.executeUpdate(MessageFormat.format(deleteUserSQL, id));
+            stmt.setLong(1, id);
+            stmt.executeUpdate();
         } catch (SQLException sqlException) {
             errorOut(sqlException);
         }
     }
 
     public List<User> getAllUsers() {
-        String getAllUsersSQL = MessageFormat.format("SELECT * FROM {0}", usersTableName);
+        String getAllUsersSQL = "SELECT * FROM my_users";
         List<User> usersList = new ArrayList<>();
         try (Connection connection = Util.getMySQLConnection();
-             PreparedStatement stmt = connection.prepareStatement(getAllUsersSQL)) {
+            PreparedStatement stmt = connection.prepareStatement(getAllUsersSQL)) {
             ResultSet result = stmt.executeQuery();
             while (result.next()) {
                 User u = new User(result.getString("name"), result.getString("last_name"), result.getByte("age"));
@@ -78,7 +79,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        String clearTableSQL = MessageFormat.format("TRUNCATE TABLE {0}", usersTableName);
+        String clearTableSQL = "TRUNCATE TABLE my_users";
         try (Connection connection = Util.getMySQLConnection();
              Statement stmt = connection.createStatement()) {
             stmt.execute(clearTableSQL);
